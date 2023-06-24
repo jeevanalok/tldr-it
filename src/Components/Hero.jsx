e5a37bc300f4ae1e36551c36a452223611039af3
Jeevan Alok <inventorjeevan@gmail.com>
2023-06-24 21:46:33 +0530

    content changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -17,11 +17,11 @@ const Hero = () => {
         </button>
       </nav>
       <h1 className="head_text">
-        Summarize Articles with <br className="max-md:hidden" />
+        Just TLDR Boring Articles with <br className="max-md:hidden" />
         <span className="blue_gradient">OpenAI GPT</span>
       </h1>
       <h2 className="desc">
-        Simplify your reading with Summize,an open-source article summarizer
+        An article too long! too boring! Leave the hassle to TlDR-It ,open-source article summarizer
         that transforms lengthy articles into clear and concise summaries
       </h2>
     </header>
