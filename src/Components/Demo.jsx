fc004a1a29fa2986a7f15bc9225b9be40fb34f42
jeevanPrime369 <jeevan@primecaves.com>
2023-06-24 20:30:38 +0530

    base files

---
+++ src/Components/Demo.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+import { copy, linkIcon, loader, tick } from "../assets";
+
+import { useGetSummaryMutation } from "../services/article";
+
+const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const [getSummary, { isLoading, isError, isSuccess }] =
+    useGetSummaryMutation();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const postData = new URLSearchParams({
+      url: article.url,
+      percentage: "10",
+    });
+    const { data } = await getSummary(postData);
+
+    if (data?.summary) {
+      setArticle({ ...article, summary: data.summary });
+    }
+
+    console.log(data.summary);
+  };
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2 items">
+        <form
+          action=""
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a url:"
+            value={article.url}
+            onChange={(e) => {
+              setArticle({ ...article, url: e.target.value });
+            }}
+            className="url_input peer"
+            required
+          ></input>
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            Extract ↵
+          </button>
+        </form>
+        <h3 className="blue_gradient font-bold self-center text-xl">OR</h3>
+
+        <form action="" className="relative flex flex-col " onSubmit={() => {}}>
+          <div className="flex flex-row relative justify-center items-center">
+            <img
+              src={copy}
+              alt="link"
+              className="absolute my-2 left-0 ml-3 w-5"
+            />
+
+            <textarea
+              placeholder="Paste article text"
+              className=" url_input w-full"
+            ></textarea>
+          </div>
+          <button
+            type="Submit"
+            onClick={() => {}}
+            className="black_btn self-center my-5"
+          >
+            Summarise
+          </button>
+        </form>
+      </div>
+
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isLoading ? (
+          <img
+            src={loader}
+            alt="loader"
+            className="w-20 h-20 object-contain"
+          ></img>
+        ) : isError ? (
+          <p className="font-inter font-bold text-black text-center">
+            Error {isError}
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3 h-2">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="orange_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p>{article.summary}</p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Demo;
---
+++ src/services/article.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const rapidAPIKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+
+export const articleApi = createApi({
+  reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://text-summarize-pro.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", rapidAPIKey);
+      headers.set("X-RapidAPI-Host", "text-summarize-pro.p.rapidapi.com");
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getSummary: builder.mutation({
+      query(body) {
+        return { url: `/summarizeFromUrl`, method: "POST", body };
+      },
+    }),
+  }),
+});
+
+export const { useGetSummaryMutation } = articleApi;
